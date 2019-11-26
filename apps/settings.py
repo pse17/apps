4041cc8f69d759e7d3fa11dadf0e8fd9e7f98188
pse17 <fqibymeksm@protonmail.com>
2019-11-26 15:50:37 +0500

    requirements.txt add

--- apps/settings.py
+++ apps/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v*p_54yrfuv=r$(fs_u%n&lw)w&_4tkki)33y2hw46gu01rgv)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
